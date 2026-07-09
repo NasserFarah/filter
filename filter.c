61f7a34f019cc915fcbabf5ff62491ad1bdd0a47
Farah <farah.s.nasser@gmail.com>
2026-07-09 14:37:50 +0300

    improved line reading and filtering

--- filter.c
+++ filter.c
@@ -1,60 +1,87 @@
-#include <unistd.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
 
-#define BUFFER 100000
+#ifndef BUFFER_SIZE
+#define BUFFER_SIZE 42
+#endif
 
-void  filter(char *flt)
+char    *get_next_line(int fd)
 {
-	char	red[BUFFER + 1];
-	int	i;
-	int	j;
-	int	k;
-	int	green;
-	int	flt_size;
+	if (fd < 0)
+		return (NULL);
+        static char     buff[BUFFER_SIZE + 1] = "";
+        static char     *line;
+        static int      index = 0;
+        static int      bytes = 0;
+        int             i = 0;
+        line = malloc(40000);
+        if (!line)
+                return (perror("Error"), NULL);
+        while (1)
+        {
+                if (bytes <= index)
+                {
+                        bytes = read(fd, buff, BUFFER_SIZE);
+			index = 0;
+                        if (bytes <= 0)
+				break ;
+                        buff[bytes] = 0;
+                }
+                line[i] = buff[index];
+                i++;
+                index++;
+                if (buff[index - 1] == '\n')
+                        break;
+        }
+        line[i] = 0;
+        if (i == 0)
+                return (free(line), NULL);
+        return (line);
+}
 
-	flt_size = strlen(flt);
-	green = 1;
-	while (green > 0)
+int	main(int ac, char **av)
+{
+	ssize_t	i = 0;
+	size_t	j = 0;
+	size_t	k = 0;
+	char	*line;
+	if (!av[1] || strlen(av[1]) == 0)
+		return (1);
+	while (ac == 2 && av[1] != NULL && av[1])
 	{
-		green = read(0, red, BUFFER);
-		red[green] = 0;
-		if (green == -1)
+		line = get_next_line(0);
+		if (!line)
+			break;
+		while (line[i] != 0)
 		{
-			printf("Error: ");
-			perror("");
-		}
-		k = 0;
-		j = 0;
-		i = 0;
-		while (red[i])
-		{
-			while (flt[j] == red[i + j] && red[i + j] && flt[j])
+			if (av[1][j] == line[i])
 			{
-				while (j == (flt_size - 1) && k <= flt_size - 1 && flt_size > 0)
+				while (av[1][j] != 0 && av[1][j] == line[i] && line[i] != 0)
 				{
-					red[i + k] = 42;
-					k++;
+					j++;
+					if (j == (strlen(av[1])))
+					{
+						while (k < j)
+						{
+							printf("*");
+							k++;
+							i++;
+						}
+						i = i - 1;
+						k = 0;
+					}
 				}
-				k = 0;
-				j++;
 			}
+			else
+				printf("%c", line[i]);
 			j = 0;
 			i++;
 		}
-		printf("%s", red);
-		printf("\n");
-	}
-}
-
-int  main(int ac, char **av)
-{
-	if (ac == 2 && av[1])
-	{
-		char *flt;
-		flt = av[1];
-		filter(flt);
+		i = 0;
+		j = 0;
+		k = 0;
 	}
 	return (1);
 }
